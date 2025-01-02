50468f66ab7679bd5fb7ba9f85a9d8f9f94a1536
Matthew Cole <matthewcole79@gmail.com>
2025-01-01 23:31:11 -0500

    Call API to Fetch Weather Data Using fetch()

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,26 @@
+//0183fc1c307502fcc46e2be6c5e6f556
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = '0183fc1c307502fcc46e2be6c5e6f556'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8457;</p>  
+                              <p>Weather: ${data.weather[0].description}</p>`; // use &units=metric in URL and &#8451 for Celsius
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
